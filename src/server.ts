1c4d95d2289db630186de119665bb1c679965fd0
Phen19 <phenriquelgsilva@gmail.com>
2023-01-26 14:24:50 -0300

    dia2: rota de criação de habito e consulta de dia

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,13 @@
 import Fastify from "fastify";
 import cors from '@fastify/cors';
-import {PrismaClient} from '@prisma/client'
+import { appRoutes } from "./routes";
 
 const app = Fastify()
-const prisma = new PrismaClient()
 
 app.register(cors)
 
-app.get('/', async () => {
-    const habits = await prisma.habit.findMany({
-        where:{
-            title:{
-                startsWith: 'Beber'
-            }
-        }
-    })
+app.register(appRoutes)
 
-    return habits
-})
 
 app.listen({
     port:3333,
